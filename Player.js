55c990d3c0fb584da03273b8c1454302a784d0ec
Andi Pätzold <andreas.paetzold@contentful.com>
2021-02-19 09:58:39 +0100

    log gamestate

--- Player.js
+++ Player.js
@@ -1,13 +1,15 @@
 class Player {
   static get VERSION() {
-    return '0.1';
+    return "0.1";
   }
 
   static betRequest(gameState, bet) {
+    console.log(gameState);
     bet(0);
   }
 
   static showdown(gameState) {
+    console.log(gameState);
   }
 }
 
