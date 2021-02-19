0a4020329350b23a42a71da21aefa422591f1d50
Andi Pätzold <andreas.paetzold@contentful.com>
2021-02-19 16:20:02 +0100

    revert

--- Player.js
+++ Player.js
@@ -25,119 +25,121 @@ class Player {
   static betRequest(gameState, bet) {
     const player = gameState.players[gameState.in_action];
 
-    // try {
-    //   console.log(
-    //     "Community Cards",
-    //     JSON.stringify(gameState.community_cards, undefined, 2)
-    //   );
-    //   console.log("Players", JSON.stringify(gameState.players, undefined, 2));
-    //   console.log(
-    //     "Hole Cards",
-    //     JSON.stringify(player.hole_cards, undefined, 2)
-    //   );
+    try {
+      console.log(
+        "Community Cards",
+        JSON.stringify(gameState.community_cards, undefined, 2)
+      );
+      console.log("Players", JSON.stringify(gameState.players, undefined, 2));
+      console.log(
+        "Hole Cards",
+        JSON.stringify(player.hole_cards, undefined, 2)
+      );
 
-    //   const isSomeoneAllIn = gameState.players
-    //     .filter((_, index) => index !== gameState.in_action)
-    //     .some((p) => p.stack === 0 && p.status === "active");
-    //   if (isSomeoneAllIn) {
-    //     console.log("Someone is all in");
-    //   }
+      const isSomeoneAllIn = gameState.players
+        .filter((_, index) => index !== gameState.in_action)
+        .some((p) => p.stack === 0 && p.status === "active");
+      if (isSomeoneAllIn) {
+        console.log("Someone is all in");
+      }
 
-    //   const numberOfOpponents = gameState.players
-    //     .filter((_, index) => index !== gameState.in_action)
-    //     .filter((p) => p.state === "active").length;
+      const numberOfOpponents = gameState.players
+        .filter((_, index) => index !== gameState.in_action)
+        .filter((p) => p.state === "active").length;
 
-    //   const pair = getPair(player.hole_cards);
-    //   const probRow = pairProbability.find((p) => p.pair === pair);
+      const pair = getPair(player.hole_cards);
+      const probRow = pairProbability.find((p) => p.pair === pair);
 
-    //   const hand = Hand.solve([
-    //     ...player.hole_cards.map((card) => `${getRank(card)}${card.suit[0]}`),
-    //     ...gameState.community_cards.map(
-    //       (card) => `${getRank(card)}${card.suit[0]}`
-    //     ),
-    //   ]);
-    //   console.log(`Rank: ${hand.rank}`);
-    //   console.log(`Hand Name: ${hand.name}`);
+      const hand = Hand.solve([
+        ...player.hole_cards.map((card) => `${getRank(card)}${card.suit[0]}`),
+        ...gameState.community_cards.map(
+          (card) => `${getRank(card)}${card.suit[0]}`
+        ),
+      ]);
+      console.log(`Rank: ${hand.rank}`);
+      console.log(`Hand Name: ${hand.name}`);
 
-    //   if (isPreFlop(gameState)) {
-    //     console.log("Phase: pre-flop");
-    //     console.log(`Win Prob: ${probRow.wins}`);
-    if (probRow.wins > 15) {
+      if (isPreFlop(gameState)) {
+        console.log("Phase: pre-flop");
+        console.log(`Win Prob: ${probRow.wins}`);
+        if (probRow.wins > 20) {
+          raise(bet, gameState, player, 5);
+        } else if (probRow.wins > 16) {
+          call(bet, gameState, player);
+        } else {
+          fold(bet);
+        }
+      } else if (isPreTurn(gameState)) {
+        switch (hand.rank) {
+          case 1: // highest card
+            fold(bet);
+            break;
+          case 2: // one pair
+            raise(bet, gameState, player, 1);
+            break;
+          case 3: // two pair
+            raise(bet, gameState, player, 5);
+            break;
+          case 4: // three of a kind
+            raise(bet, gameState, player, 10);
+            break;
+          case 5: // straight
+            raise(bet, gameState, player, 15);
+            break;
+          case 6: // flush
+            raise(bet, gameState, player, 25);
+            break;
+          case 7: // full house
+          case 8: // Four of a kind
+          case 9: // Straight flush
+          case 10: // Royal flush
+            allIn(bet, player);
+            break;
+          default:
+            allIn(bet, player);
+            break;
+        }
+      } else if (isPreRiver(gameState)) {
+        switch (hand.rank) {
+          case 1: // highest card
+            fold(bet);
+            break;
+          case 2: // one pair
+            call(bet, gameState, player);
+            break;
+          case 3: // two pair
+            if (isSomeoneAllIn) {
+              call(bet, gameState, player);
+            } else {
+              raise(bet, gameState, player, 5);
+            }
+            break;
+          case 4: // three of a kind
+            raise(bet, gameState, player, 10);
+            break;
+          case 5: // straight
+            raise(bet, gameState, player, 15);
+            break;
+          case 6: // flush
+            raise(bet, gameState, player, 25);
+            break;
+          case 7: // full house
+          case 8: // Four of a kind
+          case 9: // Straight flush
+          case 10: // Royal flush
+            allIn(bet, player);
+            break;
+          default:
+            allIn(bet, player);
+            break;
+        }
+      } else {
+        call(bet, gameState, player);
+      }
+    } catch (e) {
+      console.error(e);
       bet(player.stack);
-    } else {
-      fold(bet);
     }
-    //   } else if (isPreTurn(gameState)) {
-    //     switch (hand.rank) {
-    //       case 1: // highest card
-    //         fold(bet);
-    //         break;
-    //       case 2: // one pair
-    //         raise(bet, gameState, player, 1);
-    //         break;
-    //       case 3: // two pair
-    //         raise(bet, gameState, player, 5);
-    //         break;
-    //       case 4: // three of a kind
-    //         raise(bet, gameState, player, 10);
-    //         break;
-    //       case 5: // straight
-    //         raise(bet, gameState, player, 15);
-    //         break;
-    //       case 6: // flush
-    //         raise(bet, gameState, player, 25);
-    //         break;
-    //       case 7: // full house
-    //       case 8: // Four of a kind
-    //       case 9: // Straight flush
-    //       case 10: // Royal flush
-    //         allIn(bet, player);
-    //         break;
-    //       default:
-    //         allIn(bet, player);
-    //         break;
-    //     }
-    //   } else if (isPreRiver(gameState)) {
-    //     switch (hand.rank) {
-    //       case 1: // highest card
-    //         fold(bet);
-    //         break;
-    //       case 2: // one pair
-    //         call(bet, gameState, player);
-    //         break;
-    //       case 3: // two pair
-    //         if (isSomeoneAllIn) {
-    //           call(bet, gameState, player);
-    //         } else {
-    //           raise(bet, gameState, player, 5);
-    //         }
-    //         break;
-    //       case 4: // three of a kind
-    //         raise(bet, gameState, player, 10);
-    //         break;
-    //       case 5: // straight
-    //         raise(bet, gameState, player, 15);
-    //         break;
-    //       case 6: // flush
-    //         raise(bet, gameState, player, 25);
-    //         break;
-    //       case 7: // full house
-    //       case 8: // Four of a kind
-    //       case 9: // Straight flush
-    //       case 10: // Royal flush
-    //         allIn(bet, player);
-    //         break;
-    //       default:
-    //         allIn(bet, player);
-    //         break;
-    //     }
-    //   } else {
-    //     call(bet, gameState, player);
-    //   }
-    // } catch (e) {
-    //   console.error(e);
-    bet(player.stack);
-    // }
   }
 
   static showdown(gameState) {
