741aa1c895a56c3284b520a007c6f959204feeea
Andi Pätzold <andreas.paetzold@contentful.com>
2021-02-19 10:09:34 +0100

    always bet until current_buy_in

--- Player.js
+++ Player.js
@@ -1,11 +1,19 @@
+const NAME = "Water Juggers";
+
 class Player {
   static get VERSION() {
     return "0.1";
   }
 
   static betRequest(gameState, bet) {
-    console.log(gameState);
-    bet(0);
+    try {
+      console.log(gameState);
+      const player = gameState.players.find((p) => p.name === NAME);
+      bet(gameState.current_buy_in - player.bet);
+    } catch (e) {
+      console.error(e);
+      bet(0);
+    }
   }
 
   static showdown(gameState) {
